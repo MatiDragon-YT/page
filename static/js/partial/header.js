79fdb91e1358acc27854db226b3d452e5e7de62b
MatiDragon-YT <clarosisimatias@gmail.com>
2021-08-30 18:44:12 -0300

    Update header.js

--- static/js/partial/header.js
+++ static/js/partial/header.js
@@ -1,16 +1,16 @@
 document.write(`
 	<nav>
 		<div class="nav-wrapper">
-			<a id="logo-container" href="${origen}/index.html" class="brand-logo pl-md-3">MatiDragon</a>
+			<a id="logo-container" href="${origen}index.html" class="brand-logo pl-md-3">MatiDragon</a>
 			<ul class="right hide-on-med-and-down"><b>
-				<li><a href="${origen}/index.html">Inicio</a></li>
-				<li><a href="${origen}/articles.html">Articulos</a></li>
-				<li><a href="${origen}/builds.html">Construcciones</a></li>
+				<li><a href="${origen}index.html">Inicio</a></li>
+				<li><a href="${origen}articles.html">Articulos</a></li>
+				<li><a href="${origen}builds.html">Construcciones</a></li>
 			</b></ul>
 			<ul id="nav-mobile" class="sidenav">
-				<li><a href="${origen}/index.html"><i class="material-icons">home</i>Inicio</a></li>
-				<li><a href="${origen}/articles.html"><i class="material-icons">articles</i>Articulos</a></li>
-				<li><a href="${origen}/builds.html"><i class="material-icons">build</i>Construcciones</a></li>
+				<li><a href="${origen}index.html"><i class="material-icons">home</i>Inicio</a></li>
+				<li><a href="${origen}articles.html"><i class="material-icons">articles</i>Articulos</a></li>
+				<li><a href="${origen}builds.html"><i class="material-icons">build</i>Construcciones</a></li>
 			</ul>
 			<a href="#" data-target="nav-mobile" class="top-nav sidenav-trigger waves-effect waves-light circle hide-on-large-only"><i class="material-icons black-text">menu</i></a>
 		</div>
