f86c49182ad2a3948e0c457f75c4d7041c8db6cc
MatiDragon-YT <clarosisimatias@gmail.com>
2021-08-30 18:23:57 -0300

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,4 @@
-const origen = window.location.origin + "page/static";
+const origen = window.location.origin + "page/";
 
 document.write(`
 	<meta name="Author" content="MatiDragon">
@@ -13,14 +13,14 @@ document.write(`
 	<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 	<!-- Favicons-->
 	<meta name="msapplication-TileColor" content="#FFFFFF">
-	<meta name="msapplication-TileImage" content="${origen}/images/144x144.png">
-	<link rel="apple-touch-icon-precomposed" href="${origen}/images/152x152.png">
-	<link rel="icon" href="${origen}/images/32x32.png" sizes="32x32">
-	<link rel="icon" href="${origen}/images/48x48.png" sizes="48x48" >
-	<link rel="icon" href="${origen}/images/96x96.png" sizes="96x96" >
-	<link rel="icon" href="${origen}/images/144x144.png" sizes="144x144" >
-	<link rel="shortcut icon" href="${origen}/images/favicon.ico" type="image/x-icon" >
+	<meta name="msapplication-TileImage" content="${origen}static/images/144x144.png">
+	<link rel="apple-touch-icon-precomposed" href="${origen}static/images/152x152.png">
+	<link rel="icon" href="${origen}static/images/32x32.png" sizes="32x32">
+	<link rel="icon" href="${origen}static/images/48x48.png" sizes="48x48" >
+	<link rel="icon" href="${origen}static/images/96x96.png" sizes="96x96" >
+	<link rel="icon" href="${origen}static/images/144x144.png" sizes="144x144" >
+	<link rel="shortcut icon" href="${origen}static/images/favicon.ico" type="image/x-icon" >
 	<!-- CSS-->
-	<link href="${origen}/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection,print">
+	<link href="${origen}static/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection,print">
 	<link rel="manifest" href="${origen}/manifest.json">
 `)
