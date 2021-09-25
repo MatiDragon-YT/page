eada2d1efa7a3377a1fa708ea9c1b1aaf3dba907
Matias A. Rossi <43966706+MatiDragon-YT@users.noreply.github.com>
2021-09-25 01:03:11 -0300

    Update title.js

--- static/js/partial/title.js
+++ static/js/partial/title.js
@@ -1,4 +1,9 @@
-document.write(`<h3>${doc.title()}</h3>
+document.write(`
+<main>
+<div class="section no-pad-bot" id="index-banner">
+<div class="container"><div class="row">
+<div class="col-12 col-lg-9 col-xxl-10"><markdown>
+<h3>${doc.title()}</h3>
 <h4>${doc.subtitle()}</h4>
 <p class="flow-text light">${doc.description()}</p>
 <hr>
