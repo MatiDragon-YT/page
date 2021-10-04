a6049e9567854a57ad23e8038b47b711a40975b7
Matias A. Rossi <43966706+MatiDragon-YT@users.noreply.github.com>
2021-10-04 14:50:25 -0300

    de' thodow

---
+++ static/db/articles.js
@@ -0,0 +1,30 @@
+const DATA = [
+ {
+ "id": "3",
+ "img": "code.webp",
+ "title": "Editando el main.scm - EP1; flujos y opcodes 3/3",
+ "data": "En este tutorial continuaremos explorando los hilos. Esta vez conoceremos los comandos gosub y return",
+   "tag": [
+		"tutorial", "main.scm"
+	]
+ },
+ {
+  "id": "2",
+  "img": "code.webp",
+  "title": "Editando el main.scm - EP1; flujos y opcodes 2/3",
+  "data": "En este tutorial continuaremos explorando los hilos. La lección está dedicada al lanzamiento de varios hilos y describe las reglas para los flujos entre ellos.",
+   "tag": [
+		"tutorial", "main.scm"
+	]
+ },
+ {
+  "id": "1",
+  "img": "code.webp",
+  "title": "Editando el main.scm - EP1; flujos y opcodes 1/3",
+  "data": "En este tutorial, repasaremos los conceptos básicos de la creación de scripts: hilos, comandos, etiquetas, saltos y latencia. Tambien se introducirá el concepto de los opcodes.",
+  "tag": [
+		"tutorial", "main.scm"
+	]
+ }
+]
+export default DATA;
--- static/js/highlighter/main.js
+++ static/js/highlighter/main.js
@@ -1,7 +1,4 @@
-import markdown from "./syntax/markdown.js";
-import txt from "./syntax/txt.js";
-import sanny from "./syntax/sanny.js";
-import fxt from "./syntax/fxt.js";
-import scm from "./syntax/scm.js";
-
-//$('code').addClass('bg-dark text-white rounded darken-4 p-1');
+import "./syntax/txt.js";
+import "./syntax/sanny.js";
+import "./syntax/fxt.js";
+import "./syntax/scm.js";
