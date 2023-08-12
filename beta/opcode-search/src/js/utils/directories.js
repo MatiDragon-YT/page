a4fd4ba24b2b41b074e215969b64a5befdb58f22
Matias A. Rossi <43966706+MatiDragon-YT@users.noreply.github.com>
2023-08-12 16:12:10 -0300

    Update directories.js

--- beta/opcode-search/src/js/utils/directories.js
+++ beta/opcode-search/src/js/utils/directories.js
@@ -3,8 +3,8 @@ const
 	B = location
 
 export const local = () =>
-	A == 'https://matidragon-yt.github.io/page/beta'
-		? A + '/opcode-search/'
+	A == 'https://matidragon-yt.github.io'
+		? A + '/page/beta/opcode-search/'
 		: A + '/beta/opcode-search/'
 
 export const hash = {
