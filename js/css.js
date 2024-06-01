fdf7d55d9cc135e5bafd916b1d688b1a364c810d
Matias A. Rossi <43966706+MatiDragon-YT@users.noreply.github.com>
2024-06-01 19:27:31 -0300

    add css.js

---
+++ js/css.js
@@ -0,0 +1,119 @@
+let tempCSS = ``
+  
+  	const prefixes = ['-','-sm-','-md-','-lg-','-xl-','-xxl-']
+  	const size = [0,576,768,992,1200,1400]
+  
+  	prefixes.forEach(function(prefix, resolucion){
+  		if (resolucion != 0) {tempCSS += '@media(min-width:' + size[resolucion] + 'px){'}
+  	
+  		var vIndex = 0
+  		while(vIndex < 13){
+  			tempCSS += '.cols'+prefix+vIndex+'{columns:'+vIndex+' auto}'
+  
+  			vIndex++
+  		}
+  		
+  		[
+  			['m', 'margin'],
+  			['mt', 'margin-top'],
+  			['mb', 'margin-bottom'],
+  			['ml', 'margin-left'],
+  			['mr', 'margin-right'],
+  			['mx', 'margin-block'],
+  			['my', 'margin-inline'],
+  			['p', 'padding'],
+  			['pt', 'padding-top'],
+  			['pb', 'padding-bottom'],
+  			['pl', 'padding-left'],
+  			['pr', 'padding-right'],
+  			['px', 'padding-block'],
+  			['py', 'padding-inline'],
+  			['g', 'gap'],
+  			['r', 'border-radius']
+  		].forEach(function(attribute){
+  			[
+  				0,
+  				0.25,
+  				0.5,
+  				1,
+  				1.5,
+  				3
+  			].forEach(function(value, index){
+  				tempCSS += '.' + attribute[0] + prefix + index + '{' + attribute[1]+ ':' + value + 'rem!important}';
+  			});
+  		});
+  
+  		[
+  			'auto',
+  			'8.33333333%',
+  			'16.66666667%',
+  			'25%',
+  			'33.33333333%',
+  			'41.66666667%',
+  			'50%',
+  			'58.33333333%',
+  			'66.66666667%',
+  			'75%',
+  			'83.33333333%',
+  			'91.66666667%',
+  			'100%'
+  		].forEach(function(value, index){
+  			tempCSS += '.col' + prefix + (index == 0 ? 'auto' : ++index) + '{width:' + value + '!important}'
+  			tempCSS += '.offset' + prefix + (index == 0 ? 'auto' : ++index) + '{margin-left:' + value + '!important}'
+  		});
+  
+  		for(let i = 0; i < 100; i++){
+  			tempCSS += '.w' + prefix + i + '{width:' + i + '%!important}'
+  			tempCSS += '.w' + prefix + i + 'rem{width:' + i + 'rem!important}'
+  			tempCSS += '.h' + prefix + i + '{height:' + i + 'vh!important}'
+  			tempCSS += '.opa' + prefix + i + '{opacity:' + i + '%!important}'
+  		};
+  
+  		[
+  			'flex',
+  			'none',
+  			'block',
+  			'inline-block',
+  			'grid'
+  		].forEach((value, index) => {
+  			tempCSS += `.d${prefix}${value} { display : ${value} !important}\n`
+  		});
+  
+  		[
+  			'relative',
+  			'absolute',
+  			'initial',
+  			'fixed',
+  			'reverse',
+  			'reverse-inline'
+  		].forEach((value, index) => {
+  			tempCSS += `.p${prefix}${value} { position : ${value} !important}\n`
+  		});
+  
+  		[
+  			'center',
+  			'right',
+  			'left'
+  		].forEach((value, index) => {
+  			tempCSS += `.${value}${prefix}, .${value[0]}${prefix} { text-align : ${value} !important}\n`
+  		});
+  		
+  		tempCSS += `.light${prefix} { font-weight: 300!important}\n`
+  		tempCSS += `.bold${prefix} { font-weight: 700!important}\n`
+  
+  		;[
+  			'right',
+  			'left',
+  			'top',
+  			'buttom',
+  		].forEach((value, index) => {
+  			tempCSS += `.r${prefix}${value} { ${value} : 0 !important}\n`
+  		});
+  
+  		if (resolucion != 0) {tempCSS += '}\n\n'}
+  	})
+  
+  let $cssGenerated = document.createElement('style')
+  $cssGenerated.id = 'newCss'
+  $cssGenerated.innerHTML = tempCSS
+  $('body').append($cssGenerated)
