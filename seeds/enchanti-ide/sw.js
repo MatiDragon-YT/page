39f06af2d1904600f3c00f6218beb743380c5cca
Matias A. Rossi <43966706+MatiDragon-YT@users.noreply.github.com>
2025-02-08 02:01:06 -0300

    fix?

--- seeds/enchanti-ide/sw.js
+++ seeds/enchanti-ide/sw.js
@@ -4,7 +4,7 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox
 
 // Versión actualizada del Service Worker para manejar la página offline
 
-const CACHE_NAME = "v1";
+const CACHE_NAME = "1.4.7";
 const OFFLINE_URL = "index.html";
 const ASSETS = [
   "/index.html",
@@ -13,8 +13,13 @@ const ASSETS = [
   '/manifest.json',
   '/script.js',
   '/sw.js',
-  "/data/*",
-  "/"
+  '/data/classes.db',
+  '/data/constants.txt',
+  '/data/CustomVariables.ini',
+  '/data/models.ide',
+  '/data/opcodes.txt',
+  '/data/sa.json',
+  '/data/sa_mobile.json'
   // Agrega aquí otros recursos que quieras precachear
 ];
 
@@ -28,7 +33,7 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => {
-        cache.addAll(ASSETS)
+        return cache.addAll(ASSETS)
         console.log(ASSETS)
       })
       .catch((error) => {
@@ -53,16 +58,47 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
+  // Lista de archivos que se actualizarán siempre desde la red
+  const alwaysUpdateFiles = ['/index.html', 'index.html', '/script.js', '/sw.js', '/style.css', 'style.css', '/css.js'];
+
+  // Si la solicitud es para alguno de estos archivos...
+  if (alwaysUpdateFiles.some(path => event.request.url.endsWith(path))) {
     event.respondWith(
-      fetch(event.request).catch(() => {
-        return caches.open(CACHE_NAME).then((cache) => {
-          return cache.match(OFFLINE_URL);
+      fetch(event.request)
+      .then((networkResponse) => {
+        // Abre la caché y actualiza el recurso con la versión de la red
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, networkResponse.clone());
         });
+        return networkResponse;
+      })
+      .catch(() => {
+        // En caso de error (por ejemplo, sin conexión), se intenta servir de la caché
+        return caches.match(event.request);
       })
     );
+  } else {
+    // Para otros recursos, se sigue con la estrategia actual
+    if (event.request.mode === 'navigate') {
+      event.respondWith(
+        fetch(event.request)
+        .catch(() => caches.match(OFFLINE_URL))
+      );
+    } else {
+      event.respondWith(
+        caches.match(event.request)
+        .then((cachedResponse) => {
+          return cachedResponse || fetch(event.request)
+            .then((response) => {
+              return caches.open(CACHE_NAME).then((cache) => {
+                cache.put(event.request, response.clone());
+                return response;
+              });
+            });
+        })
+      );
+    }
   }
 });
-
 
 
