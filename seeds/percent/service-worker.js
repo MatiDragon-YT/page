4a59bf8320d52151f17458bf5b440cb9438f366b
Matias A. Rossi <info.matidragon@gmail.com>
2026-02-09 16:20:12 -0300

    add Percent

---
+++ seeds/percent/service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'porcentaje-pwa-v1';
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
